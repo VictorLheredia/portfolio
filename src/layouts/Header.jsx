b4a7f32b18eb8e83490eba3a3b7e4e8f03edb3b2
VictorLHeredia <victorluizheredia@gmail.com>
2022-05-02 18:39:35 -0300

    banner ok

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -1,7 +1,76 @@
+import "../styles/Header.css";
+import { Link } from "react-scroll";
+import { Container } from "react-bootstrap";
+import { BsGithub } from "react-icons/bs";
+import { BsLinkedin } from "react-icons/bs";
+
 export default function Header() {
   return (
-    <div>
-      <p>Header</p>
+    <div id="Header">
+      <Container id="md">
+        <div className="header-container">
+          <div className="links">
+            <a
+              href="https://github.com/VictorLheredia/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <div className="link-icon">
+                <BsGithub />
+              </div>
+            </a>
+            <a
+              href="https://www.linkedin.com/in/victor-luiz-heredia-153a7b227/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <div className="link-icon">
+                <BsLinkedin />
+              </div>
+            </a>
+          </div>
+          <div className="nav-items">
+            <Link
+              className="nav-item"
+              activeClass="active"
+              to="Banner"
+              spy={true}
+              smooth={false}
+            >
+              Home
+            </Link>
+            <Link
+              className="nav-item"
+              activeClass="active"
+              to="Stacks"
+              spy={true}
+              smooth={false}
+              offset={-100}
+            >
+              Conhecimentos
+            </Link>
+            <Link
+              className="nav-item"
+              activeClass="active"
+              to="Projects"
+              spy={true}
+              smooth={false}
+              offset={-100}
+            >
+              Projetos
+            </Link>
+            <Link
+              className="nav-item"
+              activeClass="active"
+              to="Contact"
+              spy={true}
+              smooth={false}
+            >
+              Contato
+            </Link>
+          </div>
+        </div>
+      </Container>
     </div>
   );
 }
--- src/pages/Banner.jsx
+++ src/pages/Banner.jsx
@@ -1,7 +1,35 @@
+import "../styles/Banner.css";
+import { Container } from "react-bootstrap";
+import photo from "../img/photo.jpeg";
+import { Link } from "react-scroll";
+
 export default function Banner() {
   return (
-    <div>
-      <p>Banner</p>
+    <div id="Banner">
+      <Container id="md">
+        <div className="banner-container">
+          <div className="photo">
+            <img src={photo} alt="foto" />
+          </div>
+          <div className="info">
+            <div className="name">Victor Luiz Heredia</div>
+            <div className="title">Full Stack Developer</div>
+            <div className="subtitle">
+              Desenvolvedor Fullstack jr com foco em React - estudante de
+              análise e desenvolvimento de sistemas.
+            </div>
+            <Link
+              className="nav-item"
+              to="Stacks"
+              spy={true}
+              smooth={false}
+              offset={-100}
+            >
+              <button>Saiba Mais</button>
+            </Link>
+          </div>
+        </div>
+      </Container>
     </div>
   );
 }

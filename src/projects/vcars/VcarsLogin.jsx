30ae74fea9030b25788475219487bc5d0488d6f0
VictorLHeredia <victorluizheredia@gmail.com>
2022-05-03 19:43:04 -0300

    sistema de login ok

--- src/projects/vcars/VcarsLogin.jsx
+++ src/projects/vcars/VcarsLogin.jsx
@@ -1,7 +1,35 @@
+import "../../styles/VcarsLogin.css";
+
+import vcarsLogin from "../../img/projects/vcars-login.png";
+import vcarsLoginLogout from "../../img/projects/vcars-login-logout.png";
+
 export default function VcarsLogin() {
   return (
-    <div>
-      <p>Novo componente!</p>
+    <div id="VcarsLogin">
+      <div className="project-img">
+        <img src={vcarsLogin} alt="login" />
+      </div>
+      <div className="project-box">
+        <div className="project-title">Sistema de Login</div>
+        <div className="project-text">
+          Proteção da rota do painel de controle, permite acesso somente para
+          usuário <span>logado</span> e <span>autenticado</span>.
+        </div>
+        <div className="project-text">
+          Utiliza <span>JWT “JSON Web Token”</span> no backend para autenticação
+          via token.
+        </div>
+        <div className="project-text">
+          Senha do usuário no banco de dados <span>MongoDB</span> é
+          criptografada via <span>bcrypt</span>.
+        </div>
+        <div className="logout-box">
+          <img src={vcarsLoginLogout} alt="login" />
+          <div className="project-text">
+            Botão de Logout com identificação do usuário logado.
+          </div>
+        </div>
+      </div>
     </div>
   );
 }

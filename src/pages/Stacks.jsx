d41063d8fb9744742691e33c8e26622d25326c70
Victor Luiz <victorluizheredia@gmail.com>
2022-05-19 11:44:53 -0300

    add tecnlogias

--- src/pages/Stacks.jsx
+++ src/pages/Stacks.jsx
@@ -5,23 +5,28 @@ import { FiMonitor } from "react-icons/fi";
 import { FiDatabase } from "react-icons/fi";
 import { FaDraftingCompass } from "react-icons/fa";
 
-import javascript from "../img/stacks/javascript.svg";
 import react from "../img/stacks/react.svg";
+import typescript from "../img/stacks/typescript.svg";
+import javascript from "../img/stacks/javascript.svg";
 import html from "../img/stacks/html.svg";
 import css from "../img/stacks/css.svg";
+import bootstrap from "../img/stacks/bootstrap.svg";
+import axios from "../img/stacks/axios.svg";
+import nestjs from "../img/stacks/nestjs.svg";
+import adonis from "../img/stacks/adonis.svg";
+import prisma from "../img/stacks/prisma.svg";
 import node from "../img/stacks/node.svg";
 import mongo from "../img/stacks/mongodb.svg";
+import postgres from "../img/stacks/postgres.svg";
 import aws from "../img/stacks/aws.svg";
-import s3 from "../img/stacks/s3.svg";
 import git from "../img/stacks/git.svg";
 import github from "../img/stacks/github.svg";
-import bootstrap from "../img/stacks/bootstrap.svg";
 import heroku from "../img/stacks/heroku.svg";
-import axios from "../img/stacks/axios.svg";
-import bcrypt from "../img/stacks/bcrypt.svg";
-import jwt from "../img/stacks/jwt.svg";
 import vercel from "../img/stacks/vercel.svg";
 import figma from "../img/stacks/figma.svg";
+import jwt from "../img/stacks/jwt.svg";
+import s3 from "../img/stacks/s3.svg";
+import bcrypt from "../img/stacks/bcrypt.svg";
 
 export default function Stacks() {
   return (
@@ -44,6 +49,10 @@ export default function Stacks() {
                       <div className="stack">
                         <img src={react} alt="react" />
                         <div className="text">React</div>
+                      </div>
+                      <div className="stack">
+                        <img src={typescript} alt="typescript" />
+                        <div className="text">Typescript</div>
                       </div>
                       <div className="stack">
                         <img src={javascript} alt="javascript" />
@@ -80,28 +89,32 @@ export default function Stacks() {
                   <div className="stack-list-container">
                     <div className="stack-list">
                       <div className="stack">
+                        <img src={nestjs} alt="nestjs" />
+                        <div className="text">NestJs</div>
+                      </div>
+                      <div className="stack">
+                        <img src={adonis} alt="adonis" />
+                        <div className="text">Adonis</div>
+                      </div>
+                      <div className="stack">
+                        <img src={prisma} alt="prisma" />
+                        <div className="text">Prisma</div>
+                      </div>
+                      <div className="stack">
                         <img src={node} alt="node" />
-                        <div className="text">Node.js</div>
+                        <div className="text">NodeJs</div>
                       </div>
                       <div className="stack">
                         <img src={mongo} alt="mongo" />
                         <div className="text">MongoDB</div>
                       </div>
                       <div className="stack">
+                        <img src={postgres} alt="postgres" />
+                        <div className="text">Postgre SQL </div>
+                      </div>
+                      <div className="stack">
                         <img src={aws} alt="aws" />
                         <div className="text">AWS</div>
-                      </div>
-                      <div className="stack">
-                        <img src={s3} alt="s3" />
-                        <div className="text">Amazon S3</div>
-                      </div>
-                      <div className="stack">
-                        <img src={bcrypt} alt="bcrypt" />
-                        <div className="text">Bcrypt</div>
-                      </div>
-                      <div className="stack">
-                        <img src={jwt} alt="jwt" />
-                        <div className="text">JWT</div>
                       </div>
                     </div>
                   </div>
@@ -136,6 +149,14 @@ export default function Stacks() {
                       <div className="stack">
                         <img src={figma} alt="figma" />
                         <div className="text">Figma</div>
+                      </div>
+                      <div className="stack">
+                        <img src={jwt} alt="jwt" />
+                        <div className="text">JWT</div>
+                      </div>
+                      <div className="stack">
+                        <img src={s3} alt="s3" />
+                        <div className="text">Amazon S3</div>
                       </div>
                     </div>
                   </div>
